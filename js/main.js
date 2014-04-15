c86f9fb126375d5689573230c91300118c5e6e55
Zachary Lincoln <zach@bluehousegroup.com>
2014-04-15 15:59:18 -0400

    image switcher

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(function(){
 	$('.map-wrap').fitVids({ customSelector: "iframe[src*='google.com/maps']" });
+	$('[data-image-switch]').on('click', function(){
+		if($(this).find('img').attr('src') == 'images/home-bg2.png'){
+			$(this).find('img').attr('src', 'images/home-bg.png');
+		}else{
+			$(this).find('img').attr('src', 'images/home-bg2.png');
+		}
+	});
 });
