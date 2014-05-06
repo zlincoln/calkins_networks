ee7cd81e6fbb815366abf8cd559cfc13dc143e60
Zachary Lincoln <zach@bluehousegroup.com>
2014-05-06 10:03:53 -0400

    v2 rework

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
 $(function(){
 	$('.map-wrap').fitVids({ customSelector: "iframe[src*='google.com/maps']" });
-	$('.affix-container').affix({
-		top: 250
-	});
-	$('.affix-container').css({
-		width: function(){
-			return $(this).parent().width()+'px';
-		}
-	});
-	$(window).on('resize', function(){
-		$('.affix-container').css({
-			width: function(){
-				return $(this).parent().width()+'px';
-			}
-		});
-	})
+	// $('.affix-container').affix({
+	// 	offset: {
+	// 		top: 245
+	// 	}
+	// });
+	// $('.affix-container').css({
+	// 	width: function(){
+	// 		return $(this).parent().width()+'px';
+	// 	}
+	// });
+	// $(window).on('resize', function(){
+	// 	$('.affix-container').css({
+	// 		width: function(){
+	// 			return $(this).parent().width()+'px';
+	// 		}
+	// 	});
+	// });
 });
