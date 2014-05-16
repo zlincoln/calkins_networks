dd88bb48444fc98bf73d2d5b48e21115808c833c
Zachary Lincoln <zach@bluehousegroup.com>
2014-05-16 07:02:21 -0400

    postmark integration

--- index.php
+++ index.php
@@ -21,13 +21,13 @@
 		        <span class="icon-bar"></span>
 		        <span class="icon-bar"></span>
 		      </button>
-		      <a class="navbar-brand" href="index.html"><img src="images/logo-alt.png" class="img-responsive"></a>
+		      <a class="navbar-brand" href="index.php"><img src="images/logo-alt.png" class="img-responsive"></a>
 		    </div>
 		    <div class="collapse navbar-collapse" id="main-nav">
 		      <ul class="nav navbar-nav navbar-right">
-		        <li class="active"><a href="index.html">Home</a></li>
-		        <li><a href="services.html">Services</a></li>
-		        <li><a href="about-us.html">About Us</a></li>
+		        <li class="active"><a href="index.php">Home</a></li>
+		        <li><a href="services.php">Services</a></li>
+		        <li><a href="about-us.php">About Us</a></li>
 		      </ul>
 		    </div>
 		  </div>
@@ -41,8 +41,8 @@
 						<div class="row">
 							<div class="col-xs-12 col-sm-6 col-md-6">
 								<div class="cta-wrap clearfix">
-									<h3>A Partner You Can Trust</h3>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
+									<h3>Professional &amp; Knowledgeable IT Consulting Services</h3>
+									<p>Thank you for visiting Calkins Networks.  We can offer a dynamic range of IT consulting services to support your small business.  Our commitment to providing the utmost level of customer service sets us apart from our competition.</p>
 									<a href="#contact-modal" data-toggle="modal" class="btn btn-bordered">How can we help?</a>
 								</div>
 							</div>
@@ -65,7 +65,7 @@
 						<img src="images/temp/badge-it-support.png" class="img-responsive">
 						<div class="caption">
 							<h4>IT Support</h4>
-							<p>Comprehensive solutions to IT problems. On-site or remote support services - including off-hours emergency support.</p>
+							<p>Comprehensive solutions to IT problems. On-site or remote support services including off-hours emergency support.</p>
 							<a href="services.html#support" class="btn btn-primary">Learn More</a>
 						</div>
 					</div>
@@ -74,8 +74,8 @@
 					<div class="thumbnail">
 						<img src="images/temp/badge-security.png" class="img-responsive">
 						<div class="caption">
-							<h4>Security &amp; Monitoring</h4>
-							<p>Calkins Networks can provide you its expertise in regulation compliance, network protection, and development of security policies.  Daily monitoring services keep systems running efficient.</p>
+							<h4>Managed IT Services</h4>
+							<p>24/7 remote monitoring services to maximize system uptime and network infrastructure health.</p>
 							<a href="services.html#security" class="btn btn-primary">Learn More</a>
 						</div>
 					</div>
@@ -85,7 +85,7 @@
 						<img src="images/temp/badge-cloud-backup.png" class="img-responsive">
 						<div class="caption">
 							<h4>Email &amp; Cloud Backup</h4>
-							<p>Email hosting packages with spam and virus protection.  Cloud backups for all your critical data in case of distaster.</p>
+							<p>Email hosting packages with spam, virus and encryption protection.  Cloud backup services for all your critical data in case of distaster.</p>
 							<a href="services.html#backup" class="btn btn-primary">Learn More</a>
 						</div>
 					</div>
@@ -96,15 +96,15 @@
 					<img src="images/temp/badge-it-support.png" class="img-responsive">
 					<div class="caption">
 						<h4>IT Support</h4>
-						<p>Comprehensive solutions to IT problems. On-site or remote support services - including off-hours emergency support.</p>
+						<p>Comprehensive solutions to IT problems. On-site or remote support services including off-hours emergency support.</p>
 						<a href="services.html#support" class="btn btn-primary">Learn More</a>
 					</div>
 				</div>
 				<div class="thumbnail">
 					<img src="images/temp/badge-security.png" class="img-responsive">
 					<div class="caption">
-						<h4>Security &amp; Monitoring</h4>
-						<p>Calkins Networks can provide you its expertise in regulation compliance, network protection, and development of security policies.  Daily monitoring services keep systems running efficient.</p>
+						<h4>Managed IT Services</h4>
+						<p>24/7 remote monitoring services to maximize system uptime and network infrastructure health.</p>
 						<a href="services.html#security" class="btn btn-primary">Learn More</a>
 					</div>
 				</div>
@@ -112,7 +112,7 @@
 					<img src="images/temp/badge-cloud-backup.png" class="img-responsive">
 					<div class="caption">
 						<h4>Email &amp; Cloud Backup</h4>
-						<p>Email hosting packages with spam and virus protection.  Cloud backups for all your critical data in case of distaster.</p>
+						<p>Email hosting packages with spam, virus and encryption protection.  Cloud backup services for all your critical data in case of distaster.</p>
 						<a href="services.html#backup" class="btn btn-primary">Learn More</a>
 					</div>
 				</div>
@@ -169,10 +169,59 @@
 			</div>
 		</div>
 	</footer>
+	<?php
+
+		$success = false;
+
+		function spamcheck($field){
+			if($field != ''){
+				$field = filter_var($field, FILTER_SANITIZE_EMAIL);
+				if(filter_var($field, FILTER_VALIDATE_EMAIL)){
+					return true;
+				}else{
+					return false;
+				}
+			}else{
+				return false;
+			}
+		}
+		if(isset($_POST['email']) && $_POST['honeypot'] == ''){
+			require("postmark.php");
+
+			$name = isset($_POST['name']) ? $_POST['name'] : '';
+			$email = isset($_POST['email']) ? $_POST['email'] : '';
+			$message = isset($_POST['message']) ? $_POST['message'] : '';
+
+			if(spamcheck($email)){
+
+				$postmark = new Postmark("d616706e-08e8-4b02-b6a5-0a05c3431ef6", "zach@burlingtoncollective.com", $email);
+
+				$subjectLinePart = $name != '' ? $name : 'New Lead';
+
+				$messageFieldValue = $message != '' ? wordwrap($message, 70) : '[user didn\'t provide a message]';
+				$nameFieldValue = $name != '' ? $name : '[user didn\'t provide their name]';
+
+				$messageString = "Replying to this email will message your lead directly.\n\n";
+				$messageString .= "Name: ".$nameFieldValue."\n\n";
+				$messageString .= "Email: ".$email."\n\n";
+				$messageString .= "Message: ".$messageFieldValue;
+				$messageString .= "\n\n\nIf you are recieving spam often, contact your friendly neighborhood web developer: zach@burlingtoncollective.com";
+
+				$result = $postmark->to('derrick@calkinsnetworks.com')
+								->subject($subjectLinePart." via calkinsnetworks.com")
+								->plain_message($messageString)
+								->send();
+
+				$success = true;
+			}
+			
+		}
+
+	?>
 	<div id="contact-modal" class="modal fade">
 		<div class="modal-dialog">
 			<div class="modal-content">
-				<form class="form">
+				<form class="form" method="post" action="<?= $_SERVER['PHP_SELF']; ?>">
 					<div class="modal-header">
 						<button class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 						<h4 class="modal-title">Let us know how we can help you!</h4>
@@ -182,7 +231,7 @@
 							<label for="name">Name</label>
 							<input type="text" class="form-control" id="name" name="name" placeholder="Joe Smith">
 						</div>
-						<div class="form-group">
+						<div class="form-group <?= isset($email) && spamcheck($email) && !$success ? 'has-error' : ''; ?>">
 							<label for="email">Email</label>
 							<input type="email" class="form-control" id="email" name="email" placeholder="example@example.com">
 						</div>
@@ -191,6 +240,8 @@
 							<textarea rows="5" class="form-control" id="message" name="message" placeholder="Questions? Looking for a particular service?"></textarea>
 						</div>
 					</div>
+					<input type="hidden" name="honeypot">
+					<input type="hidden" id="notification" name="success" value="<?= $success ? 'success' : ''; ?>">
 					<div class="modal-footer clearfix">
 						<button type="submit" class="btn btn-primary pull-right">Submit</button>
 					</div>
@@ -198,5 +249,30 @@
 			</div>
 		</div>
 	</div>
+	<div id="success-message" class="modal fade">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<h4 class="modal-title">Thanks for reaching out!</h4>
+				</div>
+				<div class="modal-body">
+					<p>We'll get right back to you as soon as possible.</p>
+				</div>
+				<div class="modal-footer clearfix">
+					<button class="btn btn-primary" data-dismiss="modal">Close</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script type="text/javascript">
+		$(function(){
+			var notification = $('#notification').val();
+
+			if(notification == 'success'){
+				$('#success-message').modal('show');
+			}
+		})
+	</script>
 </body>
 </html>
--- services.php
+++ services.php
@@ -21,13 +21,13 @@
 		        <span class="icon-bar"></span>
 		        <span class="icon-bar"></span>
 		      </button>
-		      <a class="navbar-brand" href="index.html"><img src="images/logo-alt.png" class="img-responsive"></a>
+		      <a class="navbar-brand" href="index.php"><img src="images/logo-alt.png" class="img-responsive"></a>
 		    </div>
 		    <div class="collapse navbar-collapse" id="main-nav">
 		      <ul class="nav navbar-nav navbar-right">
-		        <li><a href="index.html">Home</a></li>
-		        <li class="active"><a href="services.html">Services</a></li>
-		        <li><a href="about-us.html">About Us</a></li>
+		        <li><a href="index.php">Home</a></li>
+		        <li class="active"><a href="services.php">Services</a></li>
+		        <li><a href="about-us.php">About Us</a></li>
 		      </ul>
 		    </div>
 		  </div>
@@ -145,13 +145,62 @@
 				</div>
 				<div class="hidden-xs col-sm-5 col-md-4">
 					<div class="affix-container">
-						<form class="form clearfix well">
+						<?php
+
+							$success = false;
+
+							function spamcheck($field){
+								if($field != ''){
+									$field = filter_var($field, FILTER_SANITIZE_EMAIL);
+									if(filter_var($field, FILTER_VALIDATE_EMAIL)){
+										return true;
+									}else{
+										return false;
+									}
+								}else{
+									return false;
+								}
+							}
+							if(isset($_POST['email']) && $_POST['honeypot'] == ''){
+								require("postmark.php");
+
+								$name = isset($_POST['name']) ? $_POST['name'] : '';
+								$email = isset($_POST['email']) ? $_POST['email'] : '';
+								$message = isset($_POST['message']) ? $_POST['message'] : '';
+
+								if(spamcheck($email)){
+
+									$postmark = new Postmark("d616706e-08e8-4b02-b6a5-0a05c3431ef6", "zach@burlingtoncollective.com", $email);
+
+									$subjectLinePart = $name != '' ? $name : 'New Lead';
+
+									$messageFieldValue = $message != '' ? wordwrap($message, 70) : '[user didn\'t provide a message]';
+									$nameFieldValue = $name != '' ? $name : '[user didn\'t provide their name]';
+
+									$messageString = "Replying to this email will message your lead directly.\n\n";
+									$messageString .= "Name: ".$nameFieldValue."\n\n";
+									$messageString .= "Email: ".$email."\n\n";
+									$messageString .= "Message: ".$messageFieldValue;
+									$messageString .= "\n\n\nIf you are recieving spam often, contact your friendly neighborhood web developer: zach@burlingtoncollective.com";
+
+									$result = $postmark->to('derrick@calkinsnetworks.com')
+													->subject($subjectLinePart." via calkinsnetworks.com")
+													->plain_message($messageString)
+													->send();
+
+									$success = true;
+								}
+								
+							}
+
+						?>
+						<form class="form clearfix well" method="post" action="<?= $_SERVER['PHP_SELF']; ?>">
 							<h3>Let us know how we can help</h3>
 							<div class="form-group">
 								<label for="name">Name</label>
 								<input type="text" class="form-control" id="name" name="name" placeholder="Joe Smith">
 							</div>
-							<div class="form-group">
+							<div class="form-group <?= isset($email) && spamcheck($email) && !$success ? 'has-error' : ''; ?>">
 								<label for="email">Email</label>
 								<input type="email" class="form-control" id="email" name="email" placeholder="example@example.com">
 							</div>
@@ -159,7 +208,9 @@
 								<label for="message">Message</label>
 								<textarea rows="5" class="form-control" id="message" name="message" placeholder="Questions? Looking for a particular service?"></textarea>
 							</div>
-							<button type="submit" class="btn btn-primary pull-right">Submit</button>
+							<input type="hidden" name="honeypot">
+							<input type="hidden" id="notification" name="success" value="<?= $success ? 'success' : ''; ?>">
+							<button type="submit" name="submit"  class="btn btn-primary pull-right">Submit</button>
 						</form>
 					</div>
 				</div>
@@ -178,7 +229,7 @@
 	<div id="contact-modal" class="modal fade">
 		<div class="modal-dialog">
 			<div class="modal-content">
-				<form class="form">
+				<form class="form" method="post" action="<?= $_SERVER['PHP_SELF']; ?>">
 					<div class="modal-header">
 						<button class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 						<h4 class="modal-title">Let us know how we can help you!</h4>
@@ -188,7 +239,7 @@
 							<label for="name">Name</label>
 							<input type="text" class="form-control" id="name" name="name" placeholder="Joe Smith">
 						</div>
-						<div class="form-group">
+						<div class="form-group <?= isset($email) && spamcheck($email) && !$success ? 'has-error' : ''; ?>">
 							<label for="email">Email</label>
 							<input type="email" class="form-control" id="email" name="email" placeholder="example@example.com">
 						</div>
@@ -197,6 +248,7 @@
 							<textarea rows="5" class="form-control" id="message" name="message" placeholder="Questions? Looking for a particular service?"></textarea>
 						</div>
 					</div>
+					<input type="hidden" name="honeypot">
 					<div class="modal-footer clearfix">
 						<button type="submit" class="btn btn-primary pull-right">Submit</button>
 					</div>
@@ -204,5 +256,30 @@
 			</div>
 		</div>
 	</div>
+	<div id="success-message" class="modal fade">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<h4 class="modal-title">Thanks for reaching out!</h4>
+				</div>
+				<div class="modal-body">
+					<p>We'll get right back to you as soon as possible.</p>
+				</div>
+				<div class="modal-footer clearfix">
+					<button class="btn btn-primary" data-dismiss="modal">Close</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script type="text/javascript">
+		$(function(){
+			var notification = $('#notification').val();
+
+			if(notification == 'success'){
+				$('#success-message').modal('show');
+			}
+		})
+	</script>
 </body>
 </html>
